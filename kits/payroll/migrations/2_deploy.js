a6c56c0baeb8ed7563476522cb54ba3c419b092d
Lisandro Corbalan <lmcorbalan@gmail.com>
2018-10-08 14:39:27 -0300

    Fix payroll kit issue exporting async function in migration

--- kits/payroll/migrations/2_deploy.js
+++ kits/payroll/migrations/2_deploy.js
@@ -7,35 +7,32 @@ const deployDAOFactory = require('@aragon/os/scripts/deploy-daofactory.js')
 
 const payrollAppId = namehash('payroll.aragonpm.eth')
 
-module.exports = async (deployer, network, accounts, arts = null) => {
-  if (arts != null) artifacts = arts // allow running outside
+module.exports = function (deployer, network, accounts, arts = null) {
+  deployer.then(async () => {
+    if (arts != null) artifacts = arts // allow running outside
 
-  const ENS = artifacts.require('@aragon/os/contracts/lib/ens/ENS.sol')
-  const PayrollKit = artifacts.require('PayrollKit')
-  const Payroll = artifacts.require('Payroll')
+    const ENS = artifacts.require('@aragon/os/contracts/lib/ens/ENS.sol')
+    const PayrollKit = artifacts.require('PayrollKit')
 
-  const ens = ENS.at(
-    process.env.ENS ||
-    '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1' // aragen's default ENS
-  )
+    const ens = ENS.at(
+      process.env.ENS ||
+      '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1' // aragen's default ENS
+    )
 
-  console.log('Before apmAddr')
+    const apmAddr = await artifacts.require('PublicResolver').at(await ens.resolver(namehash('aragonpm.eth'))).addr(namehash('aragonpm.eth'))
 
-  const apmAddr = await artifacts.require('PublicResolver').at(await ens.resolver(namehash('aragonpm.eth'))).addr(namehash('aragonpm.eth'))
+    console.log('Deploying DAOFactory...')
+    const { daoFactory } = await deployDAOFactory(null, { artifacts, verbose: false })
 
-  console.log('apmAddr', apmAddr)
+    console.log('Deploying PayrollKit...')
+    const kit = await PayrollKit.new(daoFactory.address, ens.address)
 
-  console.log('Deploying DAOFactory...')
-  const { daoFactory } = await deployDAOFactory(null, { artifacts, verbose: false })
+    console.log('Creating APM package for PayrollKit...')
+    const apm = artifacts.require('APMRegistry').at(apmAddr)
+    await apm.newRepoWithVersion('payroll-kit', accounts[0], [1, 0, 0], kit.address, 'ipfs:')
 
-  console.log('Deploying PayrollKit...')
-  const kit = await PayrollKit.new(daoFactory.address, ens.address)
+    console.log('PayrollKit:', kit.address)
 
-  console.log('Creating APM package for PayrollKit...')
-  const apm = artifacts.require('APMRegistry').at(apmAddr)
-  await apm.newRepoWithVersion('payroll-kit', accounts[0], [1, 0, 0], kit.address, 'ipfs:')
-
-  console.log('PayrollKit:', kit.address)
-
-  return kit.address
-}
+    return kit.address
+  }
+)}
